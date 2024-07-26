304bb9c8430fc1fb0fb0282410546ba425ea18dd
Marcin Chowaniec <marcin.chowaniec@outlook.com>
2024-07-26 07:43:23 +0200

    Add maven project with basic dependencies

---
+++ companies_sm/src/main/java/com/marcincho/companies_sm/CompaniesSmApplication.java
@@ -0,0 +1,13 @@
+package com.marcincho.companies_sm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompaniesSmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompaniesSmApplication.class, args);
+	}
+
+}
---
+++ companies_sm/src/test/java/com/marcincho/companies_sm/CompaniesSmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcincho.companies_sm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompaniesSmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
